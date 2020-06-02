fef2037ed56736e5e098c40745dcc912e8164802
zvezdochiot <mykaralw@yandex.ru>
2020-06-02 18:32:12 +0300

    0.2.4: use DPI

--- pdf2svg.c
+++ pdf2svg.c
@@ -33,142 +33,170 @@
 // Begin theft from ePDFview (Copyright (C) 2006 Emma's Software) under the GPL
 gchar *getAbsoluteFileName(const gchar *fileName)
 {
-	gchar *absoluteFileName = NULL;
-	if (g_path_is_absolute(fileName)) {
-		absoluteFileName = g_strdup(fileName);
-	}
-	else {
-		gchar *currentDir = g_get_current_dir();
-		absoluteFileName = g_build_filename(currentDir, fileName, NULL);
-		g_free(currentDir);
-	}
-	return absoluteFileName;
+    gchar *absoluteFileName = NULL;
+    if (g_path_is_absolute(fileName))
+    {
+        absoluteFileName = g_strdup(fileName);
+    }
+    else
+    {
+        gchar *currentDir = g_get_current_dir();
+        absoluteFileName = g_build_filename(currentDir, fileName, NULL);
+        g_free(currentDir);
+    }
+    return absoluteFileName;
 }
 // End theft from ePDFview
 
 
-int convertPage(PopplerPage *page, const char* svgFilename)
+int convertPage(PopplerPage *page, const char* svgFilename, double dpi)
 {
-	// Poppler stuff 
-	double width, height;
+    // Poppler stuff
+    double width, height;
 
-	// Cairo stuff
-	cairo_surface_t *surface;
-	cairo_t *drawcontext;
+    // Cairo stuff
+    cairo_surface_t *surface;
+    cairo_t *drawcontext;
 
-	if (page == NULL) {
-		fprintf(stderr, "Page does not exist\n");
-		return -1;
-	}
-	poppler_page_get_size (page, &width, &height);
+    if (page == NULL)
+    {
+        fprintf(stderr, "Page does not exist\n");
+        return -1;
+    }
+    poppler_page_get_size (page, &width, &height);
 
-	// Open the SVG file
-	surface = cairo_svg_surface_create(svgFilename, width, height);
-	drawcontext = cairo_create(surface);
+    // Use DPI
+    double dpi_ratio = dpi / 72.0;
 
-	// Render the PDF file into the SVG file
-	poppler_page_render_for_printing(page, drawcontext);
-	cairo_show_page(drawcontext);
+    // Open the SVG file
+    surface = cairo_svg_surface_create(svgFilename, width * dpi_ratio, height * dpi_ratio);
+    drawcontext = cairo_create(surface);
 
-	// Close the SVG file
-	cairo_destroy(drawcontext);
-	cairo_surface_destroy(surface);
+    // Use DPI
+    cairo_scale(drawcontext, dpi_ratio, dpi_ratio);
 
-	// Close the PDF file
-	g_object_unref(page);
-	
-	return 0;     
+    // Render the PDF file into the SVG file
+    poppler_page_render_for_printing(page, drawcontext);
+    cairo_show_page(drawcontext);
+
+    // Close the SVG file
+    cairo_destroy(drawcontext);
+    cairo_surface_destroy(surface);
+
+    // Close the PDF file
+    g_object_unref(page);
+
+    return 0;
 }
 
 int main(int argn, char *args[])
 {
-	// Poppler stuff
-	PopplerDocument *pdffile;
-	PopplerPage *page;
+    // Poppler stuff
+    PopplerDocument *pdffile;
+    PopplerPage *page;
+    double dpi = 72.0;
 
-	// Initialise the GType library
-	g_type_init ();
+    // Initialise the GType library
+    g_type_init ();
 
-	// Get command line arguments
-	if ((argn < 3)||(argn > 4)) {
-		printf("Usage: pdf2svg <in file.pdf> <out file.svg> [<page no>]\n");
-		return -2;
-	}
-	gchar *absoluteFileName = getAbsoluteFileName(args[1]);
-	gchar *filename_uri = g_filename_to_uri(absoluteFileName, NULL, NULL);
-	gchar *pageLabel = NULL;
+    // Get command line arguments
+    if ((argn < 3)||(argn > 5))
+    {
+        printf("Usage: pdf2svg <in file.pdf> <out file.svg> [<dpi=72>] [<page no>]\n");
+        return -2;
+    }
+    gchar *absoluteFileName = getAbsoluteFileName(args[1]);
+    gchar *filename_uri = g_filename_to_uri(absoluteFileName, NULL, NULL);
+    gchar *pageLabel = NULL;
 
-	char* svgFilename = args[2];
+    char* svgFilename = args[2];
 
-	g_free(absoluteFileName);
-	if (argn == 4) {
-		// Get page number
-		pageLabel = g_strdup(args[3]);
-	}
+    g_free(absoluteFileName);
+    if (argn > 3)
+    {
+        // Get DPI
+        dpi = atof(args[3]);
+    }
+    if (argn > 4)
+    {
+        // Get page number
+        pageLabel = g_strdup(args[4]);
+    }
 
-	// Open the PDF file
-	pdffile = poppler_document_new_from_file(filename_uri, NULL, NULL);
-	g_free(filename_uri);
-	if (pdffile == NULL) {
-		fprintf(stderr, "Unable to open file\n");
-		return -3;
-	}
+    // Open the PDF file
+    pdffile = poppler_document_new_from_file(filename_uri, NULL, NULL);
+    g_free(filename_uri);
+    if (pdffile == NULL)
+    {
+        fprintf(stderr, "Unable to open file\n");
+        return -3;
+    }
 
-	int conversionErrors = 0;
-	// Get the page
+    int conversionErrors = 0;
+    // Get the page
 
-	if(pageLabel == NULL) {
-		page = poppler_document_get_page(pdffile, 0);
-		conversionErrors = convertPage(page, svgFilename);
-	}
-	else {
-		if(strcmp(pageLabel, "all") == 0) {
-			int curError;
-			int pageCount = poppler_document_get_n_pages(pdffile);
+    if(pageLabel == NULL)
+    {
+        page = poppler_document_get_page(pdffile, 0);
+        conversionErrors = convertPage(page, svgFilename, dpi);
+    }
+    else
+    {
+        if(strcmp(pageLabel, "all") == 0)
+        {
+            int curError;
+            int pageCount = poppler_document_get_n_pages(pdffile);
 
-			if(pageCount > 9999999) {
-				fprintf(stderr, "Too many pages (>9,999,999)\n");
-				return -5;
-			}
+            if(pageCount > 9999999)
+            {
+                fprintf(stderr, "Too many pages (>9,999,999)\n");
+                return -5;
+            }
 
-			size_t svgFilenameBufLen = strlen(svgFilename) + 1;
-			char *svgFilenameBuffer = (char*)malloc(svgFilenameBufLen);
-			assert(svgFilenameBuffer != NULL);
+            size_t svgFilenameBufLen = strlen(svgFilename) + 1;
+            char *svgFilenameBuffer = (char*)malloc(svgFilenameBufLen);
+            assert(svgFilenameBuffer != NULL);
 
-			int pageInd;
-			for(pageInd = 0; pageInd < pageCount; pageInd++) {
-				while (1) {
-					size_t _wr_len = snprintf(svgFilenameBuffer, svgFilenameBufLen, svgFilename, pageInd + 1);
-					if (_wr_len >= svgFilenameBufLen) {
-						svgFilenameBufLen = _wr_len + 1;
-						svgFilenameBuffer = (char*)realloc(svgFilenameBuffer, svgFilenameBufLen);
-						assert(svgFilenameBuffer != NULL);
-						continue;
-					}
-					break;
-				}
+            int pageInd;
+            for(pageInd = 0; pageInd < pageCount; pageInd++)
+            {
+                while (1)
+                {
+                    size_t _wr_len = snprintf(svgFilenameBuffer, svgFilenameBufLen, svgFilename, pageInd + 1);
+                    if (_wr_len >= svgFilenameBufLen)
+                    {
+                        svgFilenameBufLen = _wr_len + 1;
+                        svgFilenameBuffer = (char*)realloc(svgFilenameBuffer, svgFilenameBufLen);
+                        assert(svgFilenameBuffer != NULL);
+                        continue;
+                    }
+                    break;
+                }
 
-				page = poppler_document_get_page(pdffile, pageInd);
-				curError = convertPage(page, svgFilenameBuffer);
-				if(curError != 0)
-					conversionErrors = -1;
-			}
-			free(svgFilenameBuffer);
-		}
-		else {
-			page = poppler_document_get_page_by_label(pdffile, pageLabel);
-			conversionErrors = convertPage(page, svgFilename);
-			g_free(pageLabel);
-		}
-	}
+                page = poppler_document_get_page(pdffile, pageInd);
+                curError = convertPage(page, svgFilenameBuffer, dpi);
+                if(curError != 0)
+                    conversionErrors = -1;
+            }
+            free(svgFilenameBuffer);
+        }
+        else
+        {
+            page = poppler_document_get_page_by_label(pdffile, pageLabel);
+            conversionErrors = convertPage(page, svgFilename, dpi);
+            g_free(pageLabel);
+        }
+    }
 
-	g_object_unref(pdffile);
+    g_object_unref(pdffile);
 
-	if(conversionErrors != 0) {
-		return -4;
-	}
-	else {
-		return 0;
-	}
+    if(conversionErrors != 0)
+    {
+        return -4;
+    }
+    else
+    {
+        return 0;
+    }
 
 }
